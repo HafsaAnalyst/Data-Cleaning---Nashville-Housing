1abd6b4023bb22647bd222595b0996d5a0b37712
HafsaAnalyst <hafsa.saleh1821@gmail.com>
2025-10-27 07:16:50 +0500

    Add files via upload

---
+++ DataCleaningNashvilleHousing7.14.sql
@@ -0,0 +1,19 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# SQL Server Management Studio Solution File, Format Version 20.00
+VisualStudioVersion = 15.0.28307.421
+MinimumVisualStudioVersion = 10.0.40219.1
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Default|Default = Default|Default
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{3D3E7246-00A2-4509-831B-678BA3B6F5B3}.Default|Default.ActiveCfg = Default
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {D6526FF1-B6F4-415D-81DA-6574772B6EA5}
+	EndGlobalSection
+EndGlobal
